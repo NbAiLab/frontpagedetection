5872f38287bf5beb76a8c5f79a21860b5bd57d77
Freddy Wetjen <freddy.wetjen@gmail.com>
2022-09-21 16:56:55 +0200

    fixed paths

--- bundlesimulation.py
+++ bundlesimulation.py
@@ -35,15 +35,15 @@ bundlename=""
 logdir="log"
 now = datetime.now()
 
-logfile=logdir + "/frontpagelog_"+ now.strftime("%Y%m%d-%H")+ ".log"
+logfile=logdir + "/frontpagelog_full_"+ now.strftime("%Y%m%d-%H")+ ".log"
 logfp=open(logfile, "a+")
 
 
-savedir="/nfsmounts/datastore/frontpageproductionresults"
+savedir="frontpageproductionresults"
 
 
-feature_extractor = ViTFeatureExtractor.from_pretrained('vit-front-page-384-complete-v2/checkpoint-30000')
-model = ViTForImageClassification.from_pretrained('vit-front-page-384-complete-v2/checkpoint-30000')
+feature_extractor = ViTFeatureExtractor.from_pretrained('NbAiLab/vit-front-page-384-complete-v2')
+model = ViTForImageClassification.from_pretrained('NbAiLab/vit-front-page-384-complete-v2')
 #model = model.to(torch.device("cuda"))
 model.eval()
 
