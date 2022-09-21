fd59b17ae770d6db69ac379b3d98403a6fcbcdce
Freddy Wetjen <freddy.wetjen@gmail.com>
2022-09-21 17:23:30 +0200

    added path constant

--- runandclean.sh
+++ runandclean.sh
@@ -5,24 +5,27 @@ echo "Usage:$0 "
 exit
 fi
 
-antall=`find /nfsmounts/avisforsider/data -mindepth 1 -maxdepth 1 -type d -printf '1'  | wc -c`
+PRODUCTIONDATADIR="/nfsmounts/avisforsider/data"
+
+
+antall=`find ${PRODUCTIONDATADIR} -mindepth 1 -maxdepth 1 -type d -printf '1'  | wc -c`
 
 if test ${antall} -gt 0
 then
 subdir=`echo $(date '+%d%m%Y')`
 
-cd full
-python bundlesimulation.py --masterpath /nfsmounts/avisforsider/data
-cd ../top
-python bundletopsimulation.py --masterpath /nfsmounts/avisforsider/data
-cd ..
+#cd full
+python bundlesimulation.py --masterpath ${PRODUCTIONDATADIR}
+#cd ../top
+python bundletopsimulation.py --masterpath ${PRODUCTIONDATADIR}
+#cd ..
 fi
 
-if test -f  /nfsmounts/avisforsider/data/finished_top.status
+if test -f  ${PRODUCTIONDATADIR}/finished_top.status
 then
-if test -f  /nfsmounts/avisforsider/data/finished_full.status
+if test -f  ${PRODUCTIONDATADIR}/finished_full.status
 then
-echo "rm -rf /nfsmounts/avisforsider/data/*"
-rm -rf /nfsmounts/avisforsider/data/*
+echo "rm -rf ${PRODUCTIONDATADIR}/*"
+rm -rf ${PRODUCTIONDATADIR}/*
 fi
 fi
